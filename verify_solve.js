efd7ce4619af63b75307cf5e9e5faa048e4edb23
Unicorn <unicorngod117@gmail.com>
2026-04-30 23:25:32 +1000

    more more more

--- verify_solve.js
+++
@@ -1,4 +0,0 @@
-const math = require('mathjs');
-// Let's verify that `evalSolve` actually works inside CAS (new).html.
-// Oh wait, my patch DID modify `CAS (new).html` to use 20000 steps for `findRoots`.
-// Let's run playwright again to see if it output correctly.
